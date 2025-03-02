b2962d37b00e1b00eadde5e413304f39db9c7a5c
engnogueira <engnogueira@hotmail.com>
2025-03-02 13:15:54 -0300

    Inclusão das interfaces DAO - SellerDAO.java

---
+++ curso-jdbc/dao-jdbc/src/model/dao/SellerDAO.java
@@ -0,0 +1,14 @@
+package src.model.dao;
+
+import src.model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDAO {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
