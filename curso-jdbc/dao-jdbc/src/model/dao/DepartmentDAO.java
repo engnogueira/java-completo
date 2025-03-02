6a18dfe47afbb452a23ad8f26da8d96b90f2cb88
engnogueira <engnogueira@hotmail.com>
2025-03-02 13:14:25 -0300

    Inclusão das interfaces DAO - DepartmentDAO.java

---
+++ curso-jdbc/dao-jdbc/src/model/dao/DepartmentDAO.java
@@ -0,0 +1,15 @@
+package src.model.dao;
+
+import src.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDAO {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+}
