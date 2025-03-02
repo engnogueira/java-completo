2363db0a1854b37ab379f91b6b63a3e68b1c6a7a
engnogueira <engnogueira@hotmail.com>
2025-03-02 13:41:57 -0300

    Criação da classe SellerDAOImplementsJDBC que implementará a interface SellerDAO

---
+++ curso-jdbc/dao-jdbc/src/model/dao/impl/SellerDAOImplementsJDBC.java
@@ -0,0 +1,34 @@
+package src.model.dao.impl;
+
+import src.model.dao.SellerDAO;
+import src.model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDAOImplementsJDBC implements SellerDAO {
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
